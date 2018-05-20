95bf38e2a70ae712996df4b09ce6678781e9be4a
Isabelle Rosa <ibelle.pereira@hotmail.com>
2018-05-20 15:19:06 -0300

    mostra controles no inicio do jogo

--- main.lua
+++ main.lua
@@ -9,6 +9,8 @@ jogo = {
 	meteoros_atingidos = 0,
 	numero_meteoros_objetivo = 100,
 	menu_ativo = true,
+	ativo = false,
+	guia = true
 }
 
 jogo.menu = {
@@ -27,11 +29,11 @@ function logicaMenu(tecla)
 
 	if menu.jogar and tecla == 'space' then
 		jogo.menu_ativo = false
+		jogo.ativo = true
 	elseif menu.sair and tecla == 'space' then
 		love.event.quit()
 	end
 end
-
 
 function love.load()
 	--propriedades da janela
@@ -45,7 +47,7 @@ function love.load()
     nave_src = "imagens/nave.png"
 
     --instancia um objeto da classe Nave
-    nave = class_nave:new(nil, jogo.largura_tela, jogo.altura_tela, nave_src)
+    nave = class_nave:new(nil, jogo.largura_tela/2, jogo.altura_tela, nave_src)
     --menu principal
     menu = jogo.menu
 
@@ -67,7 +69,7 @@ end
  
 function love.update(dt)
 	-- se o jogo tiver começado
-	if jogo.menu_ativo == false and jogo.fim_jogo == false then
+	if jogo.menu_ativo == false and jogo.fim_jogo == false and not jogo.help then
 		nave:move(jogo.largura_tela, jogo.altura_tela)
 		nave:moveTiro()
 	end
@@ -76,12 +78,19 @@ end
 function love.keypressed(tecla)
 	-- logica do menu principal
 	if jogo.menu_ativo then
+		jogo.guia = true
 		logicaMenu(tecla)
+	-- some com o guia e comeca o jogo
+	elseif jogo.ativo and jogo.guia then
+		jogo.guia = false
 
 	--tecla de tiro com intervalo entre tiros
 	elseif tecla == "space" and (love.timer.getTime() - start) >= 0.75 then
 			nave:atira()
 			start = love.timer.getTime() 
+
+	elseif tecla == 'escape' then
+		jogo.menu_ativo = true		
 	end
 end
  
@@ -97,12 +106,13 @@ function love.draw()
 			love.graphics.draw(start_img, jogo.largura_tela/2-99, 250)
 			love.graphics.draw(exitact_img, jogo.largura_tela/2-99, 350)
 		end
-
-		--love.graphics.draw(comandos_img, jogo.largura_tela/2-115.5, 350)
-
+	--exibe controles antes do jogo começar
+	elseif jogo.guia and jogo.ativo then
+		love.graphics.draw(comandos_img, jogo.largura_tela/2-115.5, jogo.altura_tela/2-49.5)
+		love.graphics.draw(nave_img, nave.x, nave.y)
+	--jogo
 	else
 	    love.graphics.draw(nave_img, nave.x, nave.y)
-
 
 	    --desenha tiros
 	    for k, tiro in pairs(nave.tiros) do
--- nave.lua
+++ nave.lua
@@ -5,12 +5,12 @@ Nave = {
 }
 
 -- construtor
-function Nave:new (obj, largura_tela, altura_tela, img)
+function Nave:new (obj, posx, posy, img)
   obj = obj or {}
   setmetatable(obj, self)
   self.__index = self
-  self.x = largura_tela/2 - self.largura/2 or 0 --seta posição no meio da tela
-  self.y = altura_tela - self.altura or 0 --seta nave na posição inferior
+  self.x = posx - self.largura/2 or 0 --seta posição no meio da tela
+  self.y = posy - self.altura or 0 --seta nave na posição inferior
   self.imagem_src = img
   return obj
 end
