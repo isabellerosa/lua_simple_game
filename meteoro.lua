0f0d3c198a05f44bf1c9266cab7e650dd46c99a3
danilogila <danilosantana58@gmail.com>
2018-05-22 23:29:50 -0300

    Funções e caracteristicas dos Asteroides

---
+++ meteoro.lua
@@ -0,0 +1,70 @@
+-- ----------------------------------------
+-- ----------------------------------------
+-- ----------------------------------------
+-- Funções e caracteristicas dos Asteróides
+-- ----------------------------------------
+-- ----------------------------------------
+-- ----------------------------------------
+
+meteoros = {}
+
+function criaMeteoro()
+	meteoro = {
+		x = math.random(jogo.largura_tela),
+		y = -70,
+		largura = 50,
+		altura = 44,
+		peso = math.random(3),
+		deslocamento_horizontal = math.random(-1,1)
+		
+	}
+
+	table.insert(meteoros, meteoro)
+end
+
+function moveMeteoros()
+	for i,meteoro in pairs(meteoros) do
+		meteoro.y = meteoro.y + meteoro.peso
+		meteoro.x = meteoro.x + meteoro.deslocamento_horizontal
+	end
+end
+
+function temColisao(X1, Y1, L1, A1, X2, Y2, L2, A2) 
+	return  X2 < X1 + L1 and
+			X1 < X2 + L2 and
+			Y1 < Y2 + A2 and
+			Y2 < Y1 + A1
+end
+
+function trocaMusicaDeFundo()
+	love.audio.stop(musica_ambiente)
+	love.audio.play(musica_game_over)
+end
+
+function checaColisoes()
+	for k,meteoro in pairs(meteoros) do
+		if temColisao(meteoro.x, meteoro.y, meteoro.largura, meteoro.altura,
+		 nave.x, nave.y, nave.largura, nave.altura) then
+			trocaMusicaDeFundo()
+			destroiNave()
+			jogo.fim_jogo = true
+		end
+	end
+end
+
+function removeMeteoros()
+	for i = #meteoros,1,-1 do
+		if meteoros[i].y > jogo.altura_tela then
+			table.remove(meteoros, i)
+		end
+	end
+end
+
+function destroiNave()
+	
+	love.audio.play(musica_destruicao)
+	nave.imagem_src  = "imagens/tiro.png"
+	nave.imagem = love.graphics.newImage(nave.imagem_src )
+	nave.altura = 16
+	nave.largura = 16
+end
